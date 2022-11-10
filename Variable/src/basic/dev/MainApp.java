23fb10b90324727ea9a4ca997bfcabed4eceaba5
Dude699 <113655625+Dude699@users.noreply.github.com>
2022-11-10 09:44:04 +0700

    Add files via upload

---
+++ Variable/src/basic/dev/MainApp.java
@@ -0,0 +1,23 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 ** Khaibáo biến
+		 **/
+		String firstName= "Le Trung";
+		String lastName= "Nguyen";
+		int old = 18;
+		float markMath = 7f;
+		boolean passed = true;
+		System.out.println("Ho va ten:");
+		System.out.println(firstName+ " "+ lastName);
+		System.out.println("Mark Math: "+ markMath);
+		System.out.println("Passed: "+ passed);
+		//Khai báo hằng. Hằng phải viết hoa 
+		final float PI= 3.14f;
+	}
+
+}
